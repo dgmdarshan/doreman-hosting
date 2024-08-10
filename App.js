498cab7de19024269c1229d72df6fe1aa5eae434
Darshan GM <98661514+dgmdarshan@users.noreply.github.com>
2024-08-10 22:46:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+// Using p5.js for simplicity
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+    // Draw Doraemon-themed elements like clouds, stars, or a simple Doraemon character
+  }
+  
